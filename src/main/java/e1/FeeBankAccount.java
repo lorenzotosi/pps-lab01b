5dfc2eee8937bc1667c15ed8295485f2788c6c06
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-03-03 14:40:36 +0100

    refactor feeBankAccount

--- src/main/java/e1/FeeBankAccount.java
+++ src/main/java/e1/FeeBankAccount.java
@@ -3,22 +3,10 @@ package e1;
 public class FeeBankAccount implements BankAccount {
 
     private final BankAccount base;
-    private final int fee;
-    private FeeStrategy feeStrategy;
-
-    public FeeBankAccount(BankAccount base) {
-        this.base = base;
-        this.fee = 1;
-    }
-
-    public FeeBankAccount(BankAccount base, int fee) {
-        this.base = base;
-        this.fee = fee;
-    }
+    private final FeeStrategy feeStrategy;
 
     public FeeBankAccount(BankAccount base, FeeStrategy feeStrategy) {
         this.base = base;
-        this.fee = 1;
         this.feeStrategy = feeStrategy;
     }
 
