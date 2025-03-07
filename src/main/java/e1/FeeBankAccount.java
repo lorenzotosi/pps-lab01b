807ba082374e7d2e1285e70ddf62237fe82ac17b
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-03-07 16:26:23 +0100

    remove comments

--- src/main/java/e1/FeeBankAccount.java
+++ src/main/java/e1/FeeBankAccount.java
@@ -2,19 +2,16 @@ package e1;
 
 public class FeeBankAccount extends BankAccountDecorator {
 
-    //private final BankAccount base;
     private final FeeStrategy feeStrategy;
 
     public FeeBankAccount(BankAccount base, FeeStrategy feeStrategy) {
         super(base);
-        //this.base = base;
         this.feeStrategy = feeStrategy;
     }
 
 
     @Override
     public void withdraw(int amount) {
-        //base.withdraw(amount + this.feeStrategy.calculateFee(amount));
         super.withdraw(amount + this.feeStrategy.calculateFee(amount));
     }
 }
--- src/main/java/e1/WithdrawBankAccount.java
+++ src/main/java/e1/WithdrawBankAccount.java
@@ -2,12 +2,10 @@ package e1;
 
 public class WithdrawBankAccount extends BankAccountDecorator {
 
-    //private final BankAccount base;
     private final WithdrawStrategy withdrawStrategy;
 
     public WithdrawBankAccount(BankAccount base, WithdrawStrategy withdrawStrategy) {
         super(base);
-        //this.base = base;
         this.withdrawStrategy = withdrawStrategy;
     }
 
@@ -17,7 +15,6 @@ public class WithdrawBankAccount extends BankAccountDecorator {
         if (this.withdrawStrategy.checkWithdrawal(this.getBalance(), amount)) {
             throw new IllegalStateException();
         }
-        //base.withdraw(amount);
         super.withdraw(amount);
     }
 }
